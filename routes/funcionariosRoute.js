49d430e4128581fdde7b3a4c8a04f35a5ce0d838
Nayana Loivos <loivosdev@gmail.com>
2022-04-22 09:11:35 -0300

    finalizado CRUD e rotas de funcionarios

--- routes/funcionariosRoute.js
+++ routes/funcionariosRoute.js
@@ -4,5 +4,9 @@ const FuncionarioController = require('../controllers/FuncionarioController');
 const router = Router();
 
 router.get('/funcionarios', FuncionarioController.pegaTodosOsFuncionarios);
+router.get('/funcionarios/:id', FuncionarioController.PegaUmFuncionário);
+router.post('/funcionarios', FuncionarioController.criaFuncionario);
+router.put('/funcionarios/:id', FuncionarioController.atualizafuncionario);
+router.delete('/funcionarios/:id', FuncionarioController.apagaFuncionario);
 
 module.exports = router;
