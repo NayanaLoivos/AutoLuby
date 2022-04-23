bdee9c1b52be186d2e5e478d732388a70cc45238
Nayana Loivos <loivosdev@gmail.com>
2022-04-22 22:59:06 -0300

    usando novas rotas

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const funcionarios = require('./funcionariosRoute');
+const veiculos = require('./veiculosRoute');
 
 module.exports = app => {
     app.use(express.json());
     app.use(express.urlencoded( { extended: true } ) );
-    
+    app.use(funcionarios);
+    app.use(veiculos);
 }
