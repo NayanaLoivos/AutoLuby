9b5d0f63d62e829222246240ee2929a4b89aa746
Nayana Loivos <loivosdev@gmail.com>
2022-04-23 14:11:59 -0300

    add rotas de vendas e reservas

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const funcionarios = require('./funcionariosRoute');
 const veiculos = require('./veiculosRoute');
+const vendas = require('./vendasRoute');
+const reservas = require('./reservasRoute');
 
 module.exports = app => {
     app.use(express.json());
     app.use(express.urlencoded( { extended: true } ) );
     app.use(funcionarios);
     app.use(veiculos);
+    app.use(vendas);
+    app.use(reservas);
 }
